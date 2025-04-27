3ae0da59dbd112e611eb711758a318d009eae8da
Laura Zaporta <laura.zaporta.7e8@itb.cat>
2025-04-27 16:16:19 +0200

    Exercise 2 finished

--- Processos/Program.cs
+++ Processos/Program.cs
@@ -1,1 +1,56 @@
-﻿
+﻿using System.Diagnostics;
+using System.IO;
+
+public class Program()
+{
+    public static void Main()
+    {
+        const string MenuInfo = "\n EXERCISES MENU" +
+            "\n ------------- \n" +
+            "\n [1] - See README.md" +
+            "\n [2] - ExTwo() - Processes informaation" +
+            "\n [0] - Exit";
+        const string MenuChoose = "\n Choose an option: ";
+
+        bool exit = false;
+        string menuOption = string.Empty;
+
+        while (!exit) {
+
+            Console.WriteLine(MenuInfo);
+            Console.Write(MenuChoose);
+            menuOption = Console.ReadLine();
+
+            switch (menuOption)
+            {
+                case "2":
+                    ExTwo();
+                    break;
+                case "0":
+                    exit = true;
+                    break;
+                default:
+                    Console.WriteLine("\n Not valid option! Choose another one");
+                    break;
+            } 
+        }
+    }
+
+    private static void ExTwo()
+    {
+        const string Path = @"../../../files/processes.txt";
+        File.WriteAllText(Path, String.Empty);
+
+        Process[] allProcs = Process.GetProcesses();
+        Console.WriteLine("\n PROCESSES: \n ----------");
+
+        using (StreamWriter sw = new StreamWriter(Path))
+        {
+            foreach (Process process in allProcs)
+            {
+                Console.WriteLine($"\n Name: {process.ProcessName} \n PID: {process.Id}\n");
+                sw.WriteLine($"Name: {process.ProcessName} ----- PID: {process.Id}");
+            }
+        }
+    }
+}
